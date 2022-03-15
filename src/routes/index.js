c42f7f307ee23f762d2cb2bedf4ad05fd3fcf065
cold_hailstone <cksn1993@gmail.com>
2022-03-15 18:12:16 +0900

    없는 페이지를 접근할 때의 라우터 처리

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,5 +18,9 @@ export default new VueRouter({
 			path: '/signup',
 			component: () => import('@/views/SignupPage.vue'),
 		},
+		{
+			path: '*',
+			component: () => import('@/views/NotFoundPage.vue'),
+		},
 	],
 });
