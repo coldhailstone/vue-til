2957c52523866639673715e69715b7eb54e02fbb
cold_hailstone <cksn1993@gmail.com>
2022-03-16 13:56:14 +0900

    회원 가입 폼 마크업

--- src/components/common/SignupForm.vue
+++ src/components/common/SignupForm.vue
@@ -1,5 +1,19 @@
 <template>
-	<div>signup</div>
+	<form>
+		<div>
+			<label for="username">id: </label>
+			<input id="username" type="text" />
+		</div>
+		<div>
+			<label for="password">pw: </label>
+			<input id="password" type="text" />
+		</div>
+		<div>
+			<label for="nickname">nickname: </label>
+			<input id="nickname" type="text" />
+		</div>
+		<button type="submit">login</button>
+	</form>
 </template>
 
 <script>
