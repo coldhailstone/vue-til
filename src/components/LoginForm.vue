4cfcc68261d8586ea5d9d438460e0205f44a1b3a
cold_hailstone <cksn1993@gmail.com>
2022-03-16 17:36:18 +0900

    로그인 폼 데이터 바인딩과 이벤트 연결

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,19 +1,29 @@
 <template>
-	<form>
+	<form @submit.prevent="submitForm">
 		<div>
 			<label for="username">id:</label>
-			<input id="username" type="text" />
+			<input id="username" type="text" v-model="username" />
 		</div>
 		<div>
 			<label for="password">pw:</label>
-			<input id="password" type="text" />
+			<input id="password" type="text" v-model="password" />
 		</div>
 		<button type="submit">로그인</button>
 	</form>
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			username: '',
+			password: '',
+		};
+	},
+	methods: {
+		submitForm() {},
+	},
+};
 </script>
 
 <style></style>
