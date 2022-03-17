ce7328d89dbb2d4654321ed01b841530f9b89607
cold_hailstone <cksn1993@gmail.com>
2022-03-17 13:16:04 +0900

    브랜치 전환 및 변경 사항 안내

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -1,12 +1,16 @@
 <template>
 	<header>
 		<div>
-			<router-link to="/" class="logo"> TIL </router-link>
+			<router-link to="/" class="logo">
+				TIL
+				<span v-if="isUserLogin">by {{ $store.state.username }}</span>
+			</router-link>
 		</div>
 		<div class="navigations">
 			<template v-if="isUserLogin">
-				<span class="username">{{ $store.state.username }}</span>
-				<a href="javascript:;" @click="logoutUser">Logout</a>
+				<a href="javascript:;" @click="logoutUser" class="logout-button"
+					>Logout</a
+				>
 			</template>
 			<template v-else>
 				<router-link to="/login">로그인</router-link>
@@ -69,5 +73,12 @@ a.router-link-exact-active {
 }
 .username {
 	color: white;
+}
+.logout-button {
+	font-size: 14px;
+}
+a.router-link-exact-active {
+	color: white;
+	font-weight: bold;
 }
 </style>
