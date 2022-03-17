1b9d050a91ea88cc540ce840491825e3eeb8e579
cold_hailstone <cksn1993@gmail.com>
2022-03-17 14:01:22 +0900

    인터셉터를 이용한 HTTP 헤더 설정

--- src/api/common/interceptors.js
+++ src/api/common/interceptors.js
@@ -1,6 +1,9 @@
+import store from '@/store';
+
 export function setInterceptors(instance) {
 	instance.interceptors.request.use(
 		function (config) {
+			config.headers.Authorization = store.state.token;
 			return config;
 		},
 		function (error) {
