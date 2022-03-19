f73c9f41e53815d9a38e4cbd2eb68741bd15803a
cold_hailstone <cksn1993@gmail.com>
2022-03-19 13:56:08 +0900

    뷰 필터 소개 및 적용

--- src/components/posts/PostListItem.vue
+++ src/components/posts/PostListItem.vue
@@ -7,7 +7,7 @@
 			{{ item.contents }}
 		</div>
 		<div class="post-time">
-			{{ item.createdAt }}
+			{{ item.createdAt | formatDate }}
 			<i class="icon ion-md-create" @click="routEditPage"></i>
 			<i class="icon ion-md-trash" @click="deleteItem"></i>
 		</div>
@@ -22,6 +22,11 @@ export default {
 		item: {
 			type: Object,
 			required: true,
+		},
+	},
+	filters: {
+		formatDate(value) {
+			return new Date(value);
 		},
 	},
 	methods: {
