597608e161e7ebdd17e76f688886639797f56202
cold_hailstone <cksn1993@gmail.com>
2022-03-20 14:11:45 +0900

    뷰 컴포넌트 테스트 방법

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -1,8 +1,10 @@
-import { sum } from './math';
+import Vue from 'vue';
+import LoginForm from './LoginForm.vue';
 
-describe('math.js', () => {
-	test('10 + 20 = 30', () => {
-		const result = sum(10, 20);
-		expect(result).toBe(30);
+describe('LoginForm.vue', () => {
+	test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야 한다.', () => {
+		const instance = new Vue(LoginForm).$mount();
+		console.log(instance.username);
+		expect(instance.username).toBe('');
 	});
 });
