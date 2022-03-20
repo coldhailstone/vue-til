e73a7dc25eb3e07e6a23906d0dadbd3af4a3fbbb
cold_hailstone <cksn1993@gmail.com>
2022-03-20 14:18:48 +0900

    find()를 이용한 컴포넌트 HTML 요소 검색

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -2,8 +2,9 @@ import { shallowMount } from '@vue/test-utils';
 import LoginForm from './LoginForm.vue';
 
 describe('LoginForm.vue', () => {
-	test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야 한다.', () => {
+	test('ID는 이메일 형식이어야 한다,', () => {
 		const wrapper = shallowMount(LoginForm);
-		expect(wrapper.vm.username).toBe('');
+		const idInput = wrapper.find('#username');
+		console.log(idInput.html());
 	});
 });
